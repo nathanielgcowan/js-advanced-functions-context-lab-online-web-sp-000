3cc35dcbd8bcc40a28ba0699befe0d214f22d74a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-15 00:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 /* Your Code Here */
 let createEmployeeRecord = function(row){
   return {
-    firstName: row[0]
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
 
 }
